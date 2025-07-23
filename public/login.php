87ff95562b3734f2d8c91c95d9fa2ed0fe734649
AtwongereVianney <atwongerevianney@gmail.com>
2025-07-23 09:20:04 +0300

    modify header for login

--- public/login.php
+++ public/login.php
@@ -70,7 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>MMU | Login</title>
+    <title>FuelMaster | Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <style>
